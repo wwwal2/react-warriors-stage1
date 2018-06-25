c31699ef01d13b3cb081271757e1333a7f54b962
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2018-06-25 11:08:25 +0300

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,9 @@
 import React, { Component } from "react";
 import { moviesData } from "../moviesData";
-import MovieList from "./MovieList";
-import MovieListWillWatch from "./MovieListWillWatch";
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      movies: moviesData,
-      moviesWillWatch: []
-    };
-  }
-
-  addMovieToWillWatch = movie => {
-    this.setState(prevState => ({
-      moviesWillWatch: [...prevState.moviesWillWatch, movie]
-    }));
-  };
-
-  removeMovieFromWillWatch = movie => {
-    this.setState(prevState => ({
-      moviesWillWatch: prevState.moviesWillWatch.filter(
-        item => item.id !== movie.id
-      )
-    }));
-  };
-
   render() {
-    const { movies, moviesWillWatch } = this.state;
-    return (
-      <div className="container">
-        <div className="row mt-4">
-          <div className="col-9">
-            <MovieList
-              movies={movies}
-              addMovieToWillWatch={this.addMovieToWillWatch}
-              removeMovieFromWillWatch={this.removeMovieFromWillWatch}
-            />
-          </div>
-          <div className="col-3">
-            <MovieListWillWatch moviesWillWatch={moviesWillWatch} />
-          </div>
-        </div>
-      </div>
-    );
+    return <div>React Warriors Stage 1</div>;
   }
 }
 
